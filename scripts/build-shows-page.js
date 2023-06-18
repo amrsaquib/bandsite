4f009eac9a1136e6c50a69191055d29fd513e4f8
amrsaquib <amrsaquib@gmail.com>
2023-06-18 18:23:44 -0400

    one last commit, adding all new files and changes

--- scripts/build-shows-page.js
+++ scripts/build-shows-page.js
@@ -1,143 +1,126 @@
+const apiKey = "3999575c-a45f-4a17-99d2-bea70a37e6fa";
 
-/* 
-            <div class="shows-list__show">
-                <div class="shows-list__section">
-                    <p class="shows-list__label">DATE</p>
-                    <p class="shows-list__content shows-list__content--bolded"></p>
-                </div>
-                <div class="shows-list__section">
-                    <p class="shows-list__label">VENUE</p>
-                    <p class="shows-list__content"></p>
-                </div>
-                <div class="shows-list__section">
-                    <p class="shows-list__label">LOCATION</p>
-                    <p class="shows-list__content"></p>
-                </div>
-                <button class="shows-list__button">BUY TICKETS</button>
-            </div>
-*/
-
-const apiKey = "3999575c-a45f-4a17-99d2-bea70a37e6fa"
-
-
+// Function to build the show list
 function buildShowList() {
-    let showListSection = document.createElement('section')
-    showListSection.classList.add("shows-list")
+    let showListSection = document.createElement('section');
+    showListSection.classList.add("shows-list");
 
-    let showListHeader = document.createElement('h2')
-    showListHeader.classList.add("section__header")
-    showListHeader.classList.add("shows-list__header")
-    showListHeader.innerHTML = "Shows"
+    let showListHeader = document.createElement('h2');
+    showListHeader.classList.add("section__header");
+    showListHeader.classList.add("shows-list__header");
+    showListHeader.innerHTML = "Shows";
 
-    let showListContainer = document.createElement('div')
-    showListContainer.classList.add("shows-list__list-container")
+    let showListContainer = document.createElement('div');
+    showListContainer.classList.add("shows-list__list-container");
 
-    let topLabelContainer = document.createElement('div')
-    topLabelContainer.classList.add("shows-list__top-labels-container")
+    let topLabelContainer = document.createElement('div');
+    topLabelContainer.classList.add("shows-list__top-labels-container");
+
+    // Loop to create top labels for DATE, VENUE, LOCATION
     for(let labelType of ["DATE", "VENUE", "LOCATION"]) {
-        let labelContainer = document.createElement('div')
-        labelContainer.classList.add("shows-list__top-label-container")
-        let label = document.createElement('p')
-        label.classList.add("shows-list__label")
-        label.classList.add("shows-list__top-label")
-        label.innerHTML = labelType
-        labelContainer.appendChild(label)
-        topLabelContainer.appendChild(labelContainer)
+        let labelContainer = document.createElement('div');
+        labelContainer.classList.add("shows-list__top-label-container");
+        let label = document.createElement('p');
+        label.classList.add("shows-list__label");
+        label.classList.add("shows-list__top-label");
+        label.innerHTML = labelType;
+        labelContainer.appendChild(label);
+        topLabelContainer.appendChild(labelContainer);
     }
 
-    showListContainer.appendChild(topLabelContainer)
+    showListContainer.appendChild(topLabelContainer);
 
+    let showList = document.createElement('div');
+    showList.classList.add("shows-list__list");
 
-    let showList = document.createElement('div')
-    showList.classList.add("shows-list__list")
+    showListContainer.appendChild(showList);
 
-    showListContainer.appendChild(showList)
+    showListSection.appendChild(showListHeader);
+    showListSection.appendChild(showListContainer);
 
-    showListSection.appendChild(showListHeader)
-    showListSection.appendChild(showListContainer)
+    document.querySelector("main").appendChild(showListSection);
 
-    document.querySelector("main").appendChild(showListSection)
-
+    // Fetch show data using Axios and populate the show list
     axios.get(`https://project-1-api.herokuapp.com/showdates?api_key=${apiKey}`)
     .then(r => {
-        let shows = []
-        shows = r.data
+        let shows = [];
+        shows = r.data;
         for(let show of shows) {
-            let newDate = new Date(show.date).toDateString()
-            showList.appendChild(buildShow(newDate, show.place, show.location))
+            let newDate = new Date(show.date).toDateString();
+            showList.appendChild(buildShow(newDate, show.place, show.location));
         }
     }).catch(e => {
-        console.log(e)
-    })
+        console.log(e);
+    });
 }
 
+// Function to build an individual show
 function buildShow(date, venue, location) {
-    let newShow = document.createElement('div')
-    newShow.classList.add("shows-list__show")
-    
+    let newShow = document.createElement('div');
+    newShow.classList.add("shows-list__show");
 
-    let showSectionHolder = document.createElement('div')
-    showSectionHolder.classList.add("shows-list__section")
+    let showSectionHolder = document.createElement('div');
+    showSectionHolder.classList.add("shows-list__section");
     let label = document.createElement('p');
-    label.classList.add("shows-list__label")
-    label.innerHTML = "DATE"
-    let content = document.createElement('p')
-    content.classList.add("shows-list__content")
-    content.classList.add("shows-list__content--bolded")
+    label.classList.add("shows-list__label");
+    label.innerHTML = "DATE";
+    let content = document.createElement('p');
+    content.classList.add("shows-list__content");
+    content.classList.add("shows-list__content--bolded");
     content.innerHTML = date;
-    showSectionHolder.appendChild(label)
-    showSectionHolder.appendChild(content)
-    newShow.appendChild(showSectionHolder)
-    
-    showSectionHolder = document.createElement('div')
-    showSectionHolder.classList.add("shows-list__section")
+    showSectionHolder.appendChild(label);
+    showSectionHolder.appendChild(content);
+    newShow.appendChild(showSectionHolder);
+
+    showSectionHolder = document.createElement('div');
+    showSectionHolder.classList.add("shows-list__section");
     label = document.createElement('p');
-    label.classList.add("shows-list__label")
-    label.innerHTML = "VENUE"
-    content = document.createElement('p')
-    content.classList.add("shows-list__content")
+    label.classList.add("shows-list__label");
+    label.innerHTML = "VENUE";
+    content = document.createElement('p');
+    content.classList.add("shows-list__content");
     content.innerHTML = venue;
-    showSectionHolder.appendChild(label)
-    showSectionHolder.appendChild(content)
-    newShow.appendChild(showSectionHolder)
+    showSectionHolder.appendChild(label);
+    showSectionHolder.appendChild(content);
+    newShow.appendChild(showSectionHolder);
 
-    showSectionHolder = document.createElement('div')
-    showSectionHolder.classList.add("shows-list__section")
+    showSectionHolder = document.createElement('div');
+    showSectionHolder.classList.add("shows-list__section");
     label = document.createElement('p');
-    label.classList.add("shows-list__label")
-    label.innerHTML = "LOCATION"
-    content = document.createElement('p')
-    content.classList.add("shows-list__content")
+    label.classList.add("shows-list__label");
+    label.innerHTML = "LOCATION";
+    content = document.createElement('p');
+    content.classList.add("shows-list__content");
     content.innerHTML = location;
-    showSectionHolder.appendChild(label)
-    showSectionHolder.appendChild(content)
-    newShow.appendChild(showSectionHolder)
+    showSectionHolder.appendChild(label);
+    showSectionHolder.appendChild(content);
+    newShow.appendChild(showSectionHolder);
 
-    
-    showSectionHolder = document.createElement('div')
-    showSectionHolder.classList.add("shows-list__section")
-    let button = document.createElement('button')
-    button.classList.add("button")
-    button.classList.add("shows-list__button")
-    button.innerHTML = "BUY TICKETS"
-    showSectionHolder.appendChild(button)
-    newShow.appendChild(showSectionHolder)
+    showSectionHolder = document.createElement('div');
+    showSectionHolder.classList.add("shows-list__section");
+    let button = document.createElement('button');
+    button.classList.add("button");
+    button.classList.add("shows-list__button");
+    button.innerHTML = "BUY TICKETS";
+    showSectionHolder.appendChild(button);
+    newShow.appendChild(showSectionHolder);
 
-
-    return newShow
+    return newShow;
 }
 
+// Function to handle click event on shows
 function showClicked() {
-    let lastClicked = undefined
-    let show = document.querySelector(".shows-list__list")
+    let lastClicked = undefined;
+    let show = document.querySelector(".shows-list__list");
     show.addEventListener("click", (e) => {
         if(lastClicked !== undefined) {
-            lastClicked.classList.remove("shows-list__show--clicked")
+            lastClicked.classList.remove("shows-list__show--clicked");
         }
-        lastClicked = e.target.closest(".shows-list__show")
-        lastClicked.classList.add("shows-list__show--clicked")    
-    })
+        lastClicked = e.target.closest(".shows-list__show");
+        lastClicked.classList.add("shows-list__show--clicked");
+    });
 }
 
-buildShowList()
-showClicked()
+// Call the functions to build the show list and handle clicks
+buildShowList();
+showClicked();
--- scripts/index-page.js
+++ scripts/index-page.js
@@ -1,115 +1,116 @@
- let comments = []
+const comments = [];
+const apiKey = "3999575c-a45f-4a17-99d2-bea70a37e6fa";
 
-const apiKey = "3999575c-a45f-4a17-99d2-bea70a37e6fa"
-
-
-
+// Function to add a comment
 function addComment() {
-    let form = document.querySelector(".comments__comment-box-container")
-    form.addEventListener('submit',
-    (e) => {
-        e.preventDefault()
+  const form = document.querySelector(".comments__comment-box-container");
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
 
-        if(e.target.name.value.trim() === "" || e.target.comment.value.trim === "") {
-            e.target.name.classList.add("comments__input--error")
-            e.target.comment.classList.add("comments__input--error")
-            return
-        } else {
+    const nameValue = e.target.name.value.trim();
+    const commentValue = e.target.comment.value.trim();
 
-            if(e.target.name.classList.contains("comments__input--error") && e.target.name.classList.contains("comments__input--error") ) {
-                e.target.name.classList.remove("comments__input--error")
-            e.target.comment.classList.remove("comments__input--error")
-            }
-
-            axios.post(`https://project-1-api.herokuapp.com/comments?api_key=${apiKey}`, {
-                name: e.target.name.value,
-                comment: e.target.comment.value
-            }). then(r => {
-                displayComment(r.data)
-                updateComments()
-            })
-        
-        }   
-        e.target.name.value = ""
-        e.target.comment.value = ""
-    })
-}
-
-/*
-<div class="comments__comment">
-    <div class="comments__circle"></div>
-    <div class="comments__main">
-        <div class="comments__top"><p class="comments__name"></p> <p class="comments__date"></p></div>
-        <div class="comments__content"></div>
-    </div>
-</div>
-*/
-function displayComment(comment) {
-    let n = comment.name
-    let d = new Date(comment.timestamp)
-    let c = comment.comment
-
-    let commentSection = document.querySelector(".comments__comments-list")
-
-    let newComment = document.createElement('div')
-    newComment.classList.add("comments__comment")
-
-    let greyCircle = document.createElement('div')
-    greyCircle.classList.add("comments__circle")
-
-    let mainBody = document.createElement('div')
-    mainBody.classList.add("comments__main")
-
-    let topHalf = document.createElement('div')
-    topHalf.classList.add("comments__top")
-    let name = document.createElement('p')
-    name.classList.add("comments__name")
-    name.innerHTML = n
-    let date = document.createElement('p')
-    date.classList.add("comments__date")
-    if((d.getMonth()+1) < 10) {
-        date.innerHTML = `0${d.getMonth()+1}/${d.getDate()}/${d.getFullYear()}`
+    // Validate name and comment inputs
+    if (nameValue === "" || commentValue === "") {
+      e.target.name.classList.add("comments__input--error");
+      e.target.comment.classList.add("comments__input--error");
+      return;
     } else {
-        date.innerHTML = `${d.getMonth()+1}/${d.getDate()}/${d.getFullYear()}`
+      // Remove error class if present
+      if (
+        e.target.name.classList.contains("comments__input--error") &&
+        e.target.comment.classList.contains("comments__input--error")
+      ) {
+        e.target.name.classList.remove("comments__input--error");
+        e.target.comment.classList.remove("comments__input--error");
+      }
+
+      // Send POST request to add comment
+      axios
+        .post(`https://project-1-api.herokuapp.com/comments?api_key=${apiKey}`, {
+          name: nameValue,
+          comment: commentValue,
+        })
+        .then((response) => {
+          displayComment(response.data); // Display the new comment
+          updateComments(); // Update the comments list
+        });
     }
-    topHalf.appendChild(name)
-    topHalf.appendChild(date)
 
-    let content = document.createElement('p')
-    content.classList.add("comments__content")
-    content.innerHTML = c
-
-
-    mainBody.appendChild(topHalf)
-    mainBody.appendChild(content)
-
-    newComment.appendChild(greyCircle)
-    newComment.appendChild(mainBody)
-
-    commentSection.appendChild(newComment)
+    // Clear input values
+    e.target.name.value = "";
+    e.target.comment.value = "";
+  });
 }
 
+// Function to display a comment
+function displayComment(comment) {
+  const { name, timestamp, comment: text } = comment;
+  const d = new Date(timestamp);
+
+  const commentSection = document.querySelector(".comments__comments-list");
+
+  const newComment = document.createElement("div");
+  newComment.classList.add("comments__comment");
+
+  const greyCircle = document.createElement("div");
+  greyCircle.classList.add("comments__circle");
+
+  const mainBody = document.createElement("div");
+  mainBody.classList.add("comments__main");
+
+  const topHalf = document.createElement("div");
+  topHalf.classList.add("comments__top");
+
+  const nameElement = document.createElement("p");
+  nameElement.classList.add("comments__name");
+  nameElement.textContent = name;
+
+  const dateElement = document.createElement("p");
+  dateElement.classList.add("comments__date");
+  const month = (d.getMonth() + 1).toString().padStart(2, "0");
+  const date = d.getDate();
+  const year = d.getFullYear();
+  dateElement.textContent = `${month}/${date}/${year}`;
+
+  topHalf.appendChild(nameElement);
+  topHalf.appendChild(dateElement);
+
+  const content = document.createElement("p");
+  content.classList.add("comments__content");
+  content.textContent = text;
+
+  mainBody.appendChild(topHalf);
+  mainBody.appendChild(content);
+
+  newComment.appendChild(greyCircle);
+  newComment.appendChild(mainBody);
+
+  commentSection.appendChild(newComment);
+}
+
+// Function to update the comments list
 function updateComments() {
-    axios.get(`https://project-1-api.herokuapp.com/comments?api_key=${apiKey}`)
-    .then(r => {
-        comments = r.data
-        comments.sort(function(a,b){
-            return new Date(b.timestamp) - new Date(a.timestamp);
-          });
-        console.log(comments)
-        let commentSection = document.querySelector(".comments__comments-list")
-        commentSection.innerHTML = ""
-        for(let comment of comments) {
-            displayComment(comment)
-        }
+  axios
+    .get(`https://project-1-api.herokuapp.com/comments?api_key=${apiKey}`)
+    .then((response) => {
+      const sortedComments = response.data.sort(
+        (a, b) => new Date(b.timestamp) - new Date(a.timestamp)
+      );
+      console.log(sortedComments);
+
+      const commentSection = document.querySelector(".comments__comments-list");
+      commentSection.innerHTML = "";
+
+      for (const comment of sortedComments) {
+        displayComment(comment);
+      }
     })
-    .catch(e => {
-        console.log(e)
-    })
-    
+    .catch((error) => {
+      console.log(error);
+    });
 }
 
-
-
-addComment()
-updateComments()
+// Call the functions to add comment and update comments
+addComment();
+updateComments();
